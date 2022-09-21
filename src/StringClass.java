d3b167cd7a6ea3da6d5cb7961b40c8b3caf23133
GourangJadhav <gourang.java@gmail.com>
2022-09-22 04:13:52 +0530

    gj comment

--- src/StringClass.java
+++ src/StringClass.java
@@ -4,5 +4,6 @@ public class StringClass {
 	public static void main(String[] args) {
 		System.out.println("StringClass");
 		System.out.println("Gourang jadhav");
+		System.out.println("gj");
 	}
 }
